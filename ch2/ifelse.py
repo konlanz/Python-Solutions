710a7f383e3e4cd8be213b5acd7a78740866d77d
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-05-06 11:59:29 +0000

    this is a printing false

--- ch2/ifelse.py
+++ ch2/ifelse.py
@@ -2,8 +2,10 @@ def main():
     message()
 
 def message():
-    if True:
-            print('this is a true statement') 
+    if False:
+        print('this is a true statement') 
+    else:
+        print('this is a false statement')
     
 
 if __name__ == '__main__': main()
