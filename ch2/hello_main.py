14f2203f5e2074afe31df873c081b640c15aa772
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-05-06 11:58:25 +0000

    with if only

--- ch2/hello_main.py
+++ ch2/hello_main.py
@@ -4,6 +4,7 @@ def main():
     message()
 
 def message():
-    print('This is python version {}'.format(platform.python_version()))
+    print('This is python version {}'.format(platform.python_version())) 
+    print("hello ghana")
 
 if __name__ == '__main__': main()
---
+++ ch2/ifelse.py
@@ -0,0 +1,9 @@
+def main():
+    message()
+
+def message():
+    if True:
+            print('this is a true statement') 
+    
+
+if __name__ == '__main__': main()
