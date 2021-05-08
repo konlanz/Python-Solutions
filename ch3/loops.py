a6938cb5467c304b45cf2037c1ab5185dbe5e2bb
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-05-08 00:59:25 +0000

    loops.py

---
+++ ch3/loops.py
@@ -0,0 +1,11 @@
+secret = 'swordfish'
+pw = ''
+
+while pw != secret:
+    pw = input("What's the secret word? ")
+
+
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    print(pet)
