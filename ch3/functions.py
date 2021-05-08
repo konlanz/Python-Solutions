082cc8abbb18561460675abcc18d3fd68c95a115
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-05-08 01:12:24 +0000

    another ffunction with argument

---
+++ ch3/functions.py
@@ -0,0 +1,12 @@
+def main():
+    firstZ()
+    secondZ(89)
+
+def firstZ():
+    print('hello world')
+
+#this a function with argument
+def secondZ(a): 
+    print(a)
+
+if __name__ == '__main__': main()
