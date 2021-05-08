3a5c8cfc31818de225553bc44a901dbce973bcaa
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-05-08 01:00:39 +0000

    loops done

--- ch3/loops.py
+++ ch3/loops.py
@@ -5,7 +5,7 @@ while pw != secret:
     pw = input("What's the secret word? ")
 
 
-animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+animals = ( 'cow', 'goat', 'dog', 'cat', 'donkey' )
 
 for pet in animals:
     print(pet)
